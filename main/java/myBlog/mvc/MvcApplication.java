930c802aa64957408b2654fbb59de10be93fc55b
rladhs123 <151420186+rladhs123@users.noreply.github.com>
2025-01-22 17:30:08 +0900

    .

---
+++ main/java/myBlog/mvc/MvcApplication.java
@@ -0,0 +1,13 @@
+package myBlog.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcApplication.class, args);
+	}
+
+}
